d607660467630ebd85f2d0aeddd1e272faa69265
pcw <1311137939@qq.com>
2019-12-11 22:45:43 +0800

    简易聊天室

--- WebChat/build/web/output.jsp
+++ WebChat/build/web/output.jsp
@@ -17,42 +17,11 @@
     response.setHeader("refresh", "1,url=http://localhost:8080/WebChat/output.jsp");
 %>
 
-<style>
-    .online {
-        margin-top: 8px;
-        margin-left: 35%;
-        width: 30%;
-        height: 20px;      
-        border-bottom: 1px solid grey;
-    }
-    
-    .online span {
-        margin-left: 35%;
-        font-size: 20px;
-        font-family: STXingkai,华文行楷;
-    }
-    
-    .record{
-        margin-top: 8px;
-        width: 100%;
-        height: 93%;      
-        border: 2px solid #48D1CC;
-        border-bottom: 1px solid #48D1CC;
-    }
-    
-    .record #record_textarea {
-        width: 100%;
-        height: 85%;
-        resize: none;
-        outline: none;
-        border-style:none;
-    }
-</style>
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="css/output.css" rel="stylesheet" type="text/css" />
         <title>output</title>
     </head>
     <body>
--- WebChat/src/java/com/GetPostData.java
+++ WebChat/src/java/com/GetPostData.java
@@ -21,49 +21,6 @@ public class GetPostData extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-//        使用Filter类过滤
-//        response.setContentType("text/html;charset=utf-8");
-//        request.setCharacterEncoding("utf-8");
-        
-        String remember_pwd = request.getParameter("remember_pwd");
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-
-        Cookie usernameCookie = new Cookie("usernameCookie",URLEncoder.encode(username, "UTF-8"));
-        Cookie passwordCookie = new Cookie("passwordCookie",password);
-        
-        String[] name = {"蒲承伟","丁万开","黄颖","姚晨"};
-        String[] pwd = {"201721091075","201721091073","201721091067","201721093002"};
-        String juge_name = "",juge_pwd = "";
-        boolean juge = false;
-        for(int i = 0;i < name.length;i++){
-            juge_name = name[i];
-            juge_pwd = pwd[i];
-            if(username.equals(juge_name) && password.equals(juge_pwd)){
-                juge = true;
-                request.getSession().setAttribute("nameSession", username);             //将用户名存入session中
-            }
-        }
-        
-        if(juge == true){
-            if("yes".equals(remember_pwd)){ 
-                usernameCookie.setMaxAge(60*60*24*31);
-                passwordCookie.setMaxAge(60*60*24*31);
-                response.addCookie(usernameCookie);
-                response.addCookie(passwordCookie);
-            }
-            else {
-                usernameCookie.setMaxAge(0);
-                passwordCookie.setMaxAge(0);
-                response.addCookie(usernameCookie);
-                response.addCookie(passwordCookie);
-            }
-            response.sendRedirect("loginSuccess.html");
-        }
-        else{
-            response.sendRedirect("loginFail.html");
-        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -92,7 +49,47 @@ public class GetPostData extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+//        使用Filter类过滤
+//        response.setContentType("text/html;charset=utf-8");
+//        request.setCharacterEncoding("utf-8");
+        
+        String remember_pwd = request.getParameter("remember_pwd");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        Cookie usernameCookie = new Cookie("usernameCookie",URLEncoder.encode(username, "UTF-8"));
+        Cookie passwordCookie = new Cookie("passwordCookie",password);
+        
+        String[] name = {"蒲承伟","丁万开","黄颖","姚晨"};
+        String[] pwd = {"201721091075","201721091073","201721091067","201721093002"};
+        String juge_name = "",juge_pwd = "";
+        boolean juge = false;
+        for(int i = 0;i < name.length;i++){
+            juge_name = name[i];
+            juge_pwd = pwd[i];
+            if(username.equals(juge_name) && password.equals(juge_pwd)){
+                request.getSession().setAttribute("nameSession", username);             //将用户名存入session中
+                juge = true;
+                break;
+            }
+        }
+        
+        if(juge == true){
+            if("yes".equals(remember_pwd)){ 
+                usernameCookie.setMaxAge(60*60*24*31);
+                passwordCookie.setMaxAge(60*60*24*31);
+            }
+            else {
+                usernameCookie.setMaxAge(0);
+                passwordCookie.setMaxAge(0);
+            }
+            response.addCookie(usernameCookie);
+            response.addCookie(passwordCookie);
+            response.sendRedirect("loginSuccess.html");
+        }
+        else{
+            response.sendRedirect("loginFail.html");
+        }
     }
 
     /**
